3df1ed413abd9e7f39058c97754b23c1afa5621a
Mansive <xmansive@gmail.com>
2023-05-21 20:37:03 -0500

    Prevent auto close when playing YouTube stream

--- clean-playlist.lua
+++ clean-playlist.lua
@@ -1,5 +1,5 @@
 local ext = { 
-    "mkv","mp4","webm","wmv","avi","3gp","ogv","mpg","mpeg","mov","vob" ,"ts","m2ts","divx","flv","asf","m4v","h264","h265","rmvb","rm","ogm","mp3","wav","flac","m4a","wma","ogg","opus"
+    "mkv","mp4","webm","wmv","avi","3gp","ogv","mpg","mpeg","mov","vob","ts","m2ts","divx","flv","asf","m4v","h264","h265","rmvb","rm","ogm","mp3","wav","flac","m4a","wma","ogg","opus"
 }
 
 local valid = {}
@@ -12,9 +12,10 @@ local function main()
     local skipCurrent = false
     for i = #playlist, 1, -1 do
         local ext = string.match(playlist[i].filename, "%.([^.]+)$")
+        local name = string.match(playlist[i].filename, "watch?")
         if(ext == nil) then ext = '' end
         ext = string.lower(ext)
-        if(valid[ext]) then
+        if(valid[ext] or name) then
             --do nothing
         else
             if(playlist[i].current) then
